c4fce0c397bf9cbbb1ede6d2a80da744a04514f0
Dan Jacobellis <danjacobellis@utexas.edu>
2022-10-27 01:17:43 -0500

    build

--- build.py
+++ build.py
@@ -12,7 +12,7 @@ def build():
         rmtree("jupyter_execute")
     Path("docs/.nojekyll").touch()
     
-    result = run("jupyter-nbconvert --to slides discrete_representation_learning.ipynb --output docs/discrete_representation_learning --TagRemovePreprocessor.enabled=True --TagRemovePreprocessor.remove_cell_tags='remove-nb-cell'",capture_output=True, shell=True)
+    result = run("jupyter-nbconvert --to slides discrete_representation_learning.ipynb --output docs/discrete_representation_learning --TagRemovePreprocessor.enabled=True --TagRemovePreprocessor.remove_cell_tags='remove-nb-cell' --TagRemovePreprocessor.remove_input_tags='remove-nb-input'",capture_output=True, shell=True)
     print(result.stdout.decode("utf-8"))
     
 if __name__ == '__main__':
